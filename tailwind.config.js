171a5dcc88387711ec8f17c8e168dde85c748260
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2021-07-05 00:30:38 -0400

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+      './dist/js/bundle.js',
+      'index.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
