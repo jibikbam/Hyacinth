af4becb3b34f651e307dbce93f709ea2defc35f4
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2021-07-05 14:50:29 -0400

    Test rendering multiple images

--- src/frontend/components/App.tsx
+++ src/frontend/components/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {VolumeSlice} from './VolumeSlice';
 
 function App() {
+    const [index1, setIndex1] = useState(Math.floor(Math.random() * 200));
+    const [index2, setIndex2] = useState(Math.floor(Math.random() * 200));
+
+    function handleRefreshClick() {
+        setIndex1(Math.floor(Math.random() * 200));
+        setIndex2(Math.floor(Math.random() * 200));
+    }
+
     return (
-        <VolumeSlice imagePath="data/datasets/dataset1/img1.nii.gz" sliceIndex={100} />
+        <div>
+            <button onClick={handleRefreshClick}>Refresh</button>
+            <div>
+                <VolumeSlice imagePath="data/datasets/dataset1/img1.nii.gz" sliceIndex={index1} />
+                <VolumeSlice imagePath="data/datasets/dataset1/img2.nii.gz" sliceIndex={index2} />
+            </div>
+        </div>
     )
 }
 
