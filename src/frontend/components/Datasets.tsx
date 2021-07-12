2fcaf03fdf7192cda5f8b84bc9ec925277a05e79
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2021-07-12 14:59:48 -0400

    Add icons to datasets

--- src/frontend/components/Datasets.tsx
+++ src/frontend/components/Datasets.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {PlusIcon} from '@heroicons/react/solid';
+import {ChevronRightIcon} from '@heroicons/react/outline';
 
 function loadDatasets() {
     return (window as any).dbapi.selectDatasets();
@@ -12,17 +14,24 @@ function Datasets() {
         <main className="mt-32 mx-auto max-w-screen-sm">
             <div className="flex justify-between items-end">
                 <div className="text-3xl">Datasets</div>
-                <Link to="/create-dataset/choose-directory" className="px-4 py-1.5 bg-pink-200 rounded text-black font-medium focus:outline-none focus:ring-4 ring-pink-200 ring-opacity-50">New</Link>
+                <Link
+                    to="/create-dataset/choose-directory"
+                    className="px-3 py-1.5 bg-pink-200 rounded text-black font-medium flex items-center focus:outline-none focus:ring-4 ring-pink-200 ring-opacity-50"
+                >
+                    <PlusIcon className="w-5 h-5" />
+                    <span className="ml-1">New</span>
+                </Link>
             </div>
             <div className="mt-2 space-y-3">
                 {datasets.map(d => {
                     return (
                         <Link className="block" to={`/dataset/${d.id}`}>
-                            <div className="px-3 py-2 bg-gray-700 rounded">
+                            <div className="px-3 py-2 bg-gray-700 rounded flex justify-between items-center">
                                 <div>
                                     <div className="text-2xl">{d.name}</div>
                                     <div className="text-gray-400 font-medium">{d.image_count} volumes &bull; 0 labeling sessions</div>
                                 </div>
+                                <ChevronRightIcon className="text-gray-400 w-6 h-6" />
                             </div>
                         </Link>
                     )
