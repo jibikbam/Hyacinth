671004add963da8f06f5c1c0d41fe1afccba70f4
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2021-07-19 00:22:01 -0400

    Add typed apis for frontend

---
+++ src/frontend/backend.ts
@@ -0,0 +1,21 @@
+interface DBApiType {
+    connect: () => void;
+    createTables: () => void;
+    insertDataset: (datasetName: string, rootPath: string, imageRelPaths: string) => void;
+    insertLabelingSession: (datasetId: number, sessionType: string, name: string,
+                            prompt: string, labelOptions: string, metadataJson: string) => void;
+    selectDatasets: () => any;
+    selectDataset: () => any;
+}
+
+interface FileApiType {
+    getDatasetImages: (datasetRootPath: string) => string[];
+}
+
+interface VolumeApiType {
+    readNifti: (imagePath: string) => any;
+}
+
+const dbapi = (window as any).dbapi as DBApiType;
+const fileapi = (window as any).fileapi as FileApiType;
+const volumeapi = (window as any).volumeapi as VolumeApiType;
