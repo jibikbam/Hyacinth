a9efab1ebc2a11f929e9c19d65949548175764c3
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2021-07-25 09:49:01 -0400

    Add session dropdown and clean up session overview

--- src/frontend/components/SessionOverview.tsx
+++ src/frontend/components/SessionOverview.tsx
@@ -2,23 +2,62 @@ import * as React from 'react';
 import {useEffect, useState} from 'react';
 import {dbapi, LabelingSession, Slice} from '../backend';
 import {LinkButton} from './Buttons';
-import {ChevronDownIcon, CogIcon, PlayIcon, TagIcon} from '@heroicons/react/solid';
+import {
+    ChevronDownIcon,
+    CogIcon,
+    DuplicateIcon,
+    ExternalLinkIcon,
+    PlayIcon,
+    TagIcon,
+    TrashIcon
+} from '@heroicons/react/solid';
 
 function ManageDropdown() {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
-        <button className="px-3 py-2 bg-gray-300 rounded text-black focus:outline-none focus:ring-4 ring-gray-300 ring-opacity-50 flex items-center">
-            <CogIcon className="w-5 h-5" />
-            <span className="ml-2 font-medium">Manage</span>
-            <ChevronDownIcon className="ml-4 w-5 h-5" />
-        </button>
+        <div className="relative">
+            {open && <div className="fixed top-0 left-0 w-screen h-screen" onClick={() => setOpen(false)} />}
+            <button
+                className="relative px-3 py-2 bg-gray-300 rounded text-black focus:outline-none focus:ring-4 ring-gray-300 ring-opacity-50 flex items-center"
+                onClick={() => setOpen(!open)}
+            >
+                <CogIcon className="w-5 h-5" />
+                <span className="ml-2 font-medium">Manage</span>
+                <ChevronDownIcon className="ml-4 w-5 h-5" />
+            </button>
+            {open && (
+                <div className="absolute right-0 mt-2 py-1.5 w-56 bg-gray-300 rounded font-medium overflow-hidden">
+                    <button className="w-full px-4 py-1.5 hover:bg-gray-400 focus:bg-gray-400 text-black font-medium flex items-center focus:outline-none">
+                        <ExternalLinkIcon className="w-5 h-5" />
+                        <span className="ml-2">Export Session</span>
+                    </button>
+                    <button className="w-full px-4 py-1.5 hover:bg-gray-400 focus:bg-gray-400 text-black font-medium flex items-center focus:outline-none">
+                        <ExternalLinkIcon className="w-5 h-5" />
+                        <span className="ml-2">Export Labels</span>
+                    </button>
+                    <div className="mt-1.5 pt-1.5 border-t border-gray-400" />
+                    <button className="w-full px-4 py-1.5 hover:bg-gray-400 focus:bg-gray-400 text-black font-medium flex items-center focus:outline-none">
+                        <DuplicateIcon className="w-5 h-5" />
+                        <span className="ml-2">Duplicate Session</span>
+                    </button>
+                    <button
+                        className="w-full px-4 py-1.5 hover:bg-red-700 focus:bg-red-700 text-red-700 hover:text-gray-200 focus:text-gray-200 font-medium flex items-center focus:outline-none"
+                    >
+                        <TrashIcon className="w-5 h-5" />
+                        <span className="ml-2">Delete Session</span>
+                    </button>
+                </div>
+            )}
+        </div>
     )
 }
 
 function SessionTag({children}: {children?: any}) {
     return (
-        <div className="px-3 py-1 bg-gray-800 rounded text-gray-400 flex items-center">
-            <TagIcon className="w-5 h-5" />
-            <span className="ml-1.5">{children}</span>
+        <div className="pl-2 pr-3 py-0.5 bg-gray-800 rounded text-gray-400 flex items-center">
+            <TagIcon className="w-4 h-4" />
+            <span className="ml-1.5 text-sm">{children}</span>
         </div>
     )
 }
@@ -71,8 +110,8 @@ function SessionOverview({sessionId}: {sessionId: number}) {
         <div className="px-16 pt-12 pb-8">
             <div className="flex justify-between items-start">
                 <div>
-                    <h1 className="text-4xl font-medium">{session.sessionName}</h1>
-                    <div className="mt-1 flex">
+                    <h1 className="text-5xl font-medium">{session.sessionName}</h1>
+                    <div className="mt-3 flex">
                         <SessionTag>Classification Session</SessionTag>
                     </div>
                 </div>
@@ -83,7 +122,7 @@ function SessionOverview({sessionId}: {sessionId: number}) {
             <div className="mt-6 inline-block">
                 <LinkButton to={`/label/${sessionId}/0`} color="pink">
                     <PlayIcon className="w-5 h-5" />
-                    <span className="ml-1 font-medium">Start Labeling</span>
+                    <span className="mx-1 font-medium">Start Labeling</span>
                 </LinkButton>
             </div>
             <div className="mt-4">
