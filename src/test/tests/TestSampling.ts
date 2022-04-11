8bc277f129ef41435089614a313e97a9c8f3567c
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-04-10 22:59:12 -0400

    Add session and sampling tests

--- src/test/tests/TestSampling.ts
+++ src/test/tests/TestSampling.ts
@@ -1,2 +1,33 @@
+import {dbapi} from '../../frontend/backend';
+import {SliceSampleOpts} from '../../frontend/sampling';
+import * as Sampling from '../../frontend/sampling';
+
+import * as assert from 'assert';
+import * as Fixtures from '../fixtures';
+
 export function testSliceSampling() {
+    const dataset = Fixtures.datasetFixture();
+    const images = dbapi.selectDatasetImages(dataset.id);
+
+    function sample(imageCount: number, sliceCount: number, sliceDim: number, sliceMinPct: number, sliceMaxPct: number) {
+        const sliceOpts: SliceSampleOpts = {imageCount, sliceCount, sliceDim, sliceMinPct, sliceMaxPct};
+        return Sampling.sampleSlices(images, sliceOpts);
+    }
+
+    assert.strictEqual(sample(4, 10, 0, 20, 80).length, 10);
+    assert.strictEqual(sample(1, 10, 0, 0, 100).length, 10);
+    assert.strictEqual(sample(1, 10000, 0, 0, 100).length, 176);
+    assert.throws(() => sample(0, 10, 0, 0, 100), {message: `Can't sample 0 elements`});
+    assert.throws(() => sample(4, 0, 0, 20, 80), {message: `Can't sample 0 elements`});
+    assert.throws(() => sample(4, 10, -1, 20, 80), {message: `Invalid sliceDim -1`});
+    assert.throws(() => sample(4, 10, 3, 20, 80), {message: `Invalid sliceDim 3`});
+}
+
+export function testComparisonSampling() {
+    const sample = Sampling.sampleComparisons;
+
+    assert.strictEqual(sample(10, 20).length, 20);
+    assert.throws(() => sample(10, 0), {message: `Can't sample 0 elements`});
+    assert.throws(() => sample(0, 10), {message: `Can't sample 10 elements from an array of length 0`});
+    assert.throws(() => sample(10, 100), {message: `Can't sample 100 elements from an array of length 36`});
 }
--- src/test/tests/TestSessions.ts
+++ src/test/tests/TestSessions.ts
@@ -1,8 +1,8 @@
-import * as assert from 'assert';
 import {dbapi} from '../../frontend/backend';
 import {SliceSampleOpts} from '../../frontend/sampling';
 import * as Session from '../../frontend/sessions/session';
 
+import * as assert from 'assert';
 import * as Fixtures from '../fixtures';
 
 const SLICE_OPTS: SliceSampleOpts = {
@@ -21,20 +21,113 @@ export function testClassificationSessionCreation() {
 
     const session = dbapi.selectLabelingSession(sessionId);
 
-    assert.equal(session.id, 1);
-    assert.equal(session.datasetId, dataset.id);
-    assert.equal(session.sessionType, 'Classification');
-    assert.equal(session.sessionName, 'Test session');
-    assert.equal(session.prompt, 'Test prompt!');
-    assert.equal(session.labelOptions, 'Label 1,Label2');
-    assert.equal(
-        session.metadataJson,
-        '{"Slices From":"Create New","Image Count":4,"Slice Count":10,"Slice Dim":0,"Slice Min Pct":20,"Slice Max Pct":80}'
+    assert.deepStrictEqual(
+        session,
+        {
+            id: 1,
+            datasetId: dataset.id,
+            sessionType: 'Classification',
+            sessionName: 'Test session',
+            prompt: 'Test prompt!',
+            labelOptions: 'Label 1,Label2',
+            metadataJson: '{"Slices From":"Create New","Image Count":4,"Slice Count":10,"Slice Dim":0,"Slice Min Pct":20,"Slice Max Pct":80}',
+        }
     );
+
+    const slices = dbapi.selectSessionSlices(session.id);
+
+    assert.deepStrictEqual(slices.map(s => s.elementIndex), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    for (const slice of slices) {
+        assert.strictEqual(slice.sessionId, session.id);
+        assert.strictEqual(slice.elementType, 'Slice');
+        assert.strictEqual(slice.sliceDim, 0);
+        assert.strictEqual(slice.datasetRootPath, dataset.rootPath);
+    }
+
+    const comparisons = dbapi.selectSessionComparisons(session.id);
+    assert.strictEqual(comparisons.length, 0);
 }
 
 export function testComparisonRandomSessionCreation() {
+    const dataset = Fixtures.datasetFixture();
+    const sessClass = Session.getSessionClass('ComparisonRandom');
+    const sessionId = sessClass.createSession(dataset.id, 'Test session', 'Test prompt!', 'Label 1,Label2',
+        'Create New', SLICE_OPTS, 12);
+
+    const session = dbapi.selectLabelingSession(sessionId);
+
+    assert.deepStrictEqual(
+        session,
+        {
+            id: 1,
+            datasetId: dataset.id,
+            sessionType: 'ComparisonRandom',
+            sessionName: 'Test session',
+            prompt: 'Test prompt!',
+            labelOptions: 'Label 1,Label2',
+            metadataJson: '{"Slices From":"Create New","Image Count":4,"Slice Count":10,"Slice Dim":0,"Slice Min Pct":20,"Slice Max Pct":80,"Comparison Count":12}',
+        }
+    );
+
+    const slices = dbapi.selectSessionSlices(session.id);
+
+    assert.deepStrictEqual(slices.map(s => s.elementIndex), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    for (const slice of slices) {
+        assert.strictEqual(slice.sessionId, session.id);
+        assert.strictEqual(slice.elementType, 'Slice');
+        assert.strictEqual(slice.sliceDim, 0);
+        assert.strictEqual(slice.datasetRootPath, dataset.rootPath);
+    }
+
+    const comparisons = dbapi.selectSessionComparisons(session.id);
+
+    assert.deepStrictEqual(comparisons.map(c => c.elementIndex), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    for (const comparison of comparisons) {
+        assert.strictEqual(comparison.sessionId, session.id);
+        assert.strictEqual(comparison.elementType, 'Comparison');
+        assert.strictEqual(comparison.sliceDim1, 0);
+        assert.strictEqual(comparison.sliceDim2, 0);
+        assert.strictEqual(comparison.datasetRootPath, dataset.rootPath);
+    }
 }
 
 export function testComparisonActiveSortSessionCreation() {
+    const dataset = Fixtures.datasetFixture();
+    const sessClass = Session.getSessionClass('ComparisonActiveSort');
+    const sessionId = sessClass.createSession(dataset.id, 'Test session', 'Test prompt!', 'Label 1,Label2',
+        'Create New', SLICE_OPTS, 0);
+
+    const session = dbapi.selectLabelingSession(sessionId);
+
+    assert.deepStrictEqual(
+        session,
+        {
+            id: 1,
+            datasetId: dataset.id,
+            sessionType: 'ComparisonActiveSort',
+            sessionName: 'Test session',
+            prompt: 'Test prompt!',
+            labelOptions: 'Label 1,Label2',
+            metadataJson: '{"Slices From":"Create New","Image Count":4,"Slice Count":10,"Slice Dim":0,"Slice Min Pct":20,"Slice Max Pct":80}',
+        }
+    );
+
+    const slices = dbapi.selectSessionSlices(session.id);
+
+    assert.deepStrictEqual(slices.map(s => s.elementIndex), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    for (const slice of slices) {
+        assert.strictEqual(slice.sessionId, session.id);
+        assert.strictEqual(slice.elementType, 'Slice');
+        assert.strictEqual(slice.sliceDim, 0);
+        assert.strictEqual(slice.datasetRootPath, dataset.rootPath);
+    }
+
+    const comparisons = dbapi.selectSessionComparisons(session.id);
+
+    assert.deepStrictEqual(comparisons.map(c => c.elementIndex), [0]);
+    assert.strictEqual(comparisons[0].sessionId, session.id);
+    assert.strictEqual(comparisons[0].elementType, 'Comparison');
+    assert.strictEqual(comparisons[0].sliceDim1, 0);
+    assert.strictEqual(comparisons[0].sliceDim2, 0);
+    assert.strictEqual(comparisons[0].datasetRootPath, dataset.rootPath);
 }
